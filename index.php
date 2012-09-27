aa189daee6494de44a5513e0ab63e3de7a7b7643
David Pyle <pyledw@gmail.com>
2012-09-26 20:17:06 -0500

    Newest Revision - Changed Title

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 			"http://www.w3.org/TR/html4/loose.dtd">
 			<html xmlns="http://www.w3.org/1999/xhtml">
    			<head>
-   			<title>Welcome Page</title>
+   			<title>LeaseHood - Welcome Page</title>
    			<link rel="shortcut icon" href="images/favicon.ico" />
 <?php
 	include 'Header.php';
