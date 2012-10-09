95e6dd93ed6758b24f5174e40a9793b06cfa57d4
David Pyle <pyledw@gmail.com>
2012-10-09 10:15:00 -0500

    Getting the Housing application working.  Have 3 pages, working on the rest.  Plan to finish both forms today and figure out how to compile the data.

---
+++ newHousingApplication3.php
@@ -0,0 +1,18 @@
+<?php
+    $title = "New Application #3";
+    include 'Header.php';
+    include "formElements.php";
+    ?>
+<h1 class="Title">Application Page #3</h1>
+<hr class="Title">
+<div id="mainContent">
+<?php
+    
+    createForm("90%", "90%", "Housing Application part #3", "newHousingApplication4.php");
+    
+?>
+</form>
+</div>
+<?php
+    include 'Footer.php';
+?>
