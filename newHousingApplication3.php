2fda9e59f7e7f6120b5c31cc37dd3a9479754d87
David Pyle <pyledw@gmail.com>
2012-10-23 16:10:05 -0500

    Working on pulling data if it already exist on the application - First 2 pages complete... - Stopped for a break on page 3...  trying to figure out how to update dynamic fields....  with still allowing option to add more...

--- newHousingApplication3.php
+++ newHousingApplication3.php
@@ -8,6 +8,42 @@
         header( 'Location: /loginRequired.php' ) ;
     }
     
+    require "config.inc.php";
+         
+        $con = mysql_connect($db_server,$db_user,$db_pass );
+        if(!$con)
+        {
+            die('could not connect: ' .mysql_error());
+        }
+        else
+        {
+            //echo "connected to mySQL";
+        }
+        $select = mysql_selectdb($db_database, $con);
+        if(!$select)
+        {
+            die('could not connect: ' .mysql_error());
+        }
+        else
+        {
+            //echo "Selected Database";
+        }
+        $result1 = $result = mysql_query("SELECT * FROM APPLICATION
+            WHERE UserID ='" . $_SESSION[userID] . "'");
+        $row = mysql_fetch_array($result1);
+        
+        $result = mysql_query("SELECT * FROM PREVIOUSRESIDENCE
+            WHERE ApplicationID = $row[ApplicationID] ");
+        
+        if(!$result)
+        {
+            die('could not connect: ' .mysql_error());
+        }
+        
+        
+        
+        //Will eventually test to see if the applicaiton was compleate, and go back to the next needed page.
+    
     ?>
 <h1 class="Title">Application Page #3</h1>
 <hr class="Title">
@@ -15,9 +51,19 @@
 <div id="mainContent">
     <form class="formStyle" width="90%" height="90%" method="post" action="newHousingApplication3Redirect.php">
         <div id="formerHomes">
-            <div class="formElement" id="formerHome1">
-                
-                <h3>Former Residence #1</h3><br/>
+            
+            <?php
+            if(mysql_num_fields($result) == 0)
+            {
+                while($row = mysql_fetch_array($result))
+                    {
+                       
+                    }
+            }
+            else
+            {
+                echo '<div class="formElement" id="formerHome1">              
+                <h3>Former Residence</h3><br/>
                 Type Of Residence:<select id="select" name="type1">
                     <option>
                         Owned
@@ -29,20 +75,20 @@
                         Family Friend
                     </option>
                 </select>
-                Address:<input title="THIS IS A TOOLTIP" type="text" name="address1">
-                City:<input type="text" name="city1">
-                State:<input type="text" name="state1">
-                Zip Code:<input  type="text" name="zipCode1">
-                months lived there:<input  type="text" name="months1">
+                Address:<input type="text" name="address1"/>
+                City:<input type="text" name="city1" />
+                State:<input type="text" name="state1"/>
+                Zip Code:<input  type="text" name="zipCode1"/>
+                months lived there:<input  type="text" name="months1"/>
                 <div id="renter">
-                Landlords First Name:<input type="text" name="landlordsFName1">
-                Landlords Last Name:<input type="text" name="landlordsLName1">
-                Phone Number:<input type="text" name="phoneNumber1">
-                Reason For Leaving:<input type="text" name="reasonForLeaving1">
-                Rent:<input type="text" name="rent1">
+                Landlords First Name:<input type="text" name="landlordsFName1" />
+                Landlords Last Name:<input type="text" name="landlordsLName1"/>
+                Phone Number:<input type="text" name="phoneNumber1"/>
+                Reason For Leaving:<input type="text" name="reasonForLeaving1"/>
+                Rent:<input type="text" name="rent1"/>
                 </div>
                 <div id="owner">
-                Mortgage:<input type="text" name="mortgage1">
+                Mortgage:<input type="text" name="mortgage1"/>
                 </div>
                 <input id="number" value="1" type="text" name="number" style="display: block; visibility: hidden" />
 
@@ -61,24 +107,24 @@
                         Family Friend
                     </option>
                 </select>
-                Address:<input title="THIS IS A TOOLTIP" type="text" name="address2">
-                City:<input type="text" name="city2">
-                State:<input type="text" name="state2">
-                Zip Code:<input  type="text" name="zipCode2">
-                months lived there:<input  type="text" name="months2">
+                Address:<input type="text" name="address2"/>
+                City:<input type="text" name="city2"/>
+                State:<input type="text" name="state2"/>
+                Zip Code:<input  type="text" name="zipCode2"/>
+                months lived there:<input  type="text" name="months2"/>
                 <div id="renter2">
-                Landlords First Name:<input type="text" name="landlordsFName2">
-                Landlords Last Name:<input type="text" name="landlordsLName2">
-                Phone Number:<input type="text" name="phoneNumber2">
-                Reason For Leaving:<input type="text" name="reasonForLeaving2">
-                Rent:<input type="text" name="rent2">
+                Landlords First Name:<input type="text" name="landlordsFName2"/>
+                Landlords Last Name:<input type="text" name="landlordsLName2"/>
+                Phone Number:<input type="text" name="phoneNumber2"/>
+                Reason For Leaving:<input type="text" name="reasonForLeaving2"/>
+                Rent:<input type="text" name="rent2"/>
                 </div>
                 <div id="owner2">
-                Mortgage:<input type="text" name="mortgage2">
+                Mortgage:<input type="text" name="mortgage2"/>
                 </div>
                 
             </div>
-            <div class="formElement" id="formerHome3" style="display:none;">
+            <div class="formElement" id="formerHome3">
                 
                 <h3>Former Residence #3</h3><br/>
                 Type Of Residence:<select id="select" name="type3">
@@ -92,20 +138,20 @@
                         Family Friend
                     </option>
                 </select>
-                Address:<input title="THIS IS A TOOLTIP" type="text" name="address3">
-                City:<input type="text" name="city3">
-                State:<input type="text" name="state3">
-                Zip Code:<input  type="text" name="zipCode3">
-                months lived there:<input  type="text" name="months3">
+                Address:<input type="text" name="address3" />
+                City:<input type="text" name="city3" />
+                State:<input type="text" name="state3"/>
+                Zip Code:<input  type="text" name="zipCode3" />
+                months lived there:<input  type="text" name="months3" />
                 <div id="renter3">
-                Landlords First Name:<input type="text" name="landlordsFName3">
-                Landlords Last Name:<input type="text" name="landlordsLName3">
-                Phone Number:<input type="text" name="phoneNumber3">
-                Reason For Leaving:<input type="text" name="reasonForLeaving3">
-                Rent:<input type="text" name="rent3">
+                Landlords First Name:<input type="text" name="landlordsFName3" />
+                Landlords Last Name:<input type="text" name="landlordsLName3" />
+                Phone Number:<input type="text" name="phoneNumber3" />
+                Reason For Leaving:<input type="text" name="reasonForLeaving3" />
+                Rent:<input type="text" name="rent3" />
                 </div>
                 <div id="owner3">
-                Mortgage:<input type="text" name="mortgage3">
+                Mortgage:<input type="text" name="mortgage3" />
                 </div>
 
             </div>
@@ -123,26 +169,157 @@
                         Family Friend
                     </option>
                 </select>
-                Address:<input title="THIS IS A TOOLTIP" type="text" name="address4">
-                City:<input type="text" name="city4">
-                State:<input type="text" name="state4">
-                Zip Code:<input  type="text" name="zipCode4">
-                months lived there:<input  type="text" name="months4">
+                Address:<input type="text" name="address4" />
+                City:<input type="text" name="city4" />
+                State:<input type="text" name="state4" />
+                Zip Code:<input  type="text" name="zipCode4" />
+                months lived there:<input  type="text" name="months4" />
                 <div id="renter4">
-                Landlords First Name:<input type="text" name="landlordsFName4">
-                Landlords Last Name:<input type="text" name="landlordsLName4">
-                Phone Number:<input type="text" name="phoneNumber4">
-                Reason For Leaving:<input type="text" name="reasonForLeaving4">
-                Rent:<input type="text" name="rent4">
+                Landlords First Name:<input type="text" name="landlordsFName4" />
+                Landlords Last Name:<input type="text" name="landlordsLName4" />
+                Phone Number:<input type="text" name="phoneNumber4" />
+                Reason For Leaving:<input type="text" name="reasonForLeaving4" />
+                Rent:<input type="text" name="rent4" />
                 </div>
                 <div id="owner4">
-                Mortgage:<input type="text" name="mortgage4">
+                Mortgage:<input type="text" name="mortgage4" />
                 </div>
 
             </div>
         </div>
         <font class="button" id="addFormerHome">Add another home</font>
-        <font class="button"  id="removeFormerHome">Remove home</font><br/><br/>
+        <font class="button"  id="removeFormerHome">Remove home</font><br/><br/>';
+            }
+                
+            ?>
+            <!--
+            <div class="formElement" id="formerHome1">              
+                <h3>Former Residence</h3><br/>
+                Type Of Residence:<select id="select" name="type1">
+                    <option>
+                        Owned
+                    </option>
+                    <option>
+                        Rented
+                    </option>
+                    <option>
+                        Family Friend
+                    </option>
+                </select>
+                Address:<input type="text" name="address1"/>
+                City:<input type="text" name="city1" />
+                State:<input type="text" name="state1"/>
+                Zip Code:<input  type="text" name="zipCode1"/>
+                months lived there:<input  type="text" name="months1"/>
+                <div id="renter">
+                Landlords First Name:<input type="text" name="landlordsFName1" />
+                Landlords Last Name:<input type="text" name="landlordsLName1"/>
+                Phone Number:<input type="text" name="phoneNumber1"/>
+                Reason For Leaving:<input type="text" name="reasonForLeaving1"/>
+                Rent:<input type="text" name="rent1"/>
+                </div>
+                <div id="owner">
+                Mortgage:<input type="text" name="mortgage1"/>
+                </div>
+                <input id="number" value="1" type="text" name="number" style="display: block; visibility: hidden" />
+
+            </div>
+            <div class="formElement" id="formerHome2" style="display:none;">
+                
+                <h3>Former Residence #2</h3><br/>
+                Type Of Residence:<select id="select" name="type2">
+                    <option>
+                        Owned
+                    </option>
+                    <option>
+                        Rented
+                    </option>
+                    <option>
+                        Family Friend
+                    </option>
+                </select>
+                Address:<input type="text" name="address2"/>
+                City:<input type="text" name="city2"/>
+                State:<input type="text" name="state2"/>
+                Zip Code:<input  type="text" name="zipCode2"/>
+                months lived there:<input  type="text" name="months2"/>
+                <div id="renter2">
+                Landlords First Name:<input type="text" name="landlordsFName2"/>
+                Landlords Last Name:<input type="text" name="landlordsLName2"/>
+                Phone Number:<input type="text" name="phoneNumber2"/>
+                Reason For Leaving:<input type="text" name="reasonForLeaving2"/>
+                Rent:<input type="text" name="rent2"/>
+                </div>
+                <div id="owner2">
+                Mortgage:<input type="text" name="mortgage2"/>
+                </div>
+                
+            </div>
+            <div class="formElement" id="formerHome3">
+                
+                <h3>Former Residence #3</h3><br/>
+                Type Of Residence:<select id="select" name="type3">
+                    <option>
+                        Owned
+                    </option>
+                    <option>
+                        Rented
+                    </option>
+                    <option>
+                        Family Friend
+                    </option>
+                </select>
+                Address:<input type="text" name="address3" />
+                City:<input type="text" name="city3" />
+                State:<input type="text" name="state3"/>
+                Zip Code:<input  type="text" name="zipCode3" />
+                months lived there:<input  type="text" name="months3" />
+                <div id="renter3">
+                Landlords First Name:<input type="text" name="landlordsFName3" />
+                Landlords Last Name:<input type="text" name="landlordsLName3" />
+                Phone Number:<input type="text" name="phoneNumber3" />
+                Reason For Leaving:<input type="text" name="reasonForLeaving3" />
+                Rent:<input type="text" name="rent3" />
+                </div>
+                <div id="owner3">
+                Mortgage:<input type="text" name="mortgage3" />
+                </div>
+
+            </div>
+            <div class="formElement" id="formerHome4" style="display:none;">
+                
+                <h3>Former Residence #4</h3><br/>
+                Type Of Residence:<select id="select" name="type4">
+                    <option>
+                        Owned
+                    </option>
+                    <option>
+                        Rented
+                    </option>
+                    <option>
+                        Family Friend
+                    </option>
+                </select>
+                Address:<input type="text" name="address4" />
+                City:<input type="text" name="city4" />
+                State:<input type="text" name="state4" />
+                Zip Code:<input  type="text" name="zipCode4" />
+                months lived there:<input  type="text" name="months4" />
+                <div id="renter4">
+                Landlords First Name:<input type="text" name="landlordsFName4" />
+                Landlords Last Name:<input type="text" name="landlordsLName4" />
+                Phone Number:<input type="text" name="phoneNumber4" />
+                Reason For Leaving:<input type="text" name="reasonForLeaving4" />
+                Rent:<input type="text" name="rent4" />
+                </div>
+                <div id="owner4">
+                Mortgage:<input type="text" name="mortgage4" />
+                </div>
+
+            </div>
+        </div>
+            -->
+        
         <h3>Personal History</h3>
         
         Have you ever been convicted of a felony?
@@ -159,10 +336,10 @@
         
 
         <h3>Credit History</h3>
-        Total outstanding balance on consumer debt (credit cards, charge accounts, etc.): <input type="text" name="devitCardDebt" value=""/><br/>
-        Total monthly payment on all credit/loan accounts: <input type="text" name="monthlyPayments" value=""/><br/>
-        Total outstanding balance on loans (auto, education, mortgage, etc.)<input type="text" name="loans value="/><br/>
-        Approximate total assets (cash, investments, property equity, etc.): <input type="text" name="equity" value=""/><br/>
+        Total outstanding balance on consumer debt (credit cards, charge accounts, etc.): <input type="text" name="devitCardDebt" <?php echo "value'" .$row[TotalConsumerDebt] . "'"?>/><br/>
+        Total monthly payment on all credit/loan accounts: <input type="text" name="monthlyPayments" <?php echo "value'" .$row[MonthlyDebtPayment] . "'"?>/><br/>
+        Total outstanding balance on loans (auto, education, mortgage, etc.)<input type="text" name="loans" <?php echo "value'" .$row[TotalLoanDebt] . "'"?>/><br/>
+        Approximate total assets (cash, investments, property equity, etc.): <input type="text" name="equity" <?php echo "value'" .$row[TotalAssets] . "'"?>/><br/>
         
         <br/>
         <button type="submit" class="button">Save and Continue</button>
