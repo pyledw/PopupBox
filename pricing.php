f90679f7546c463ea46ee5f359571eaee07ba0a3
a.long <longas@mail.lipscomb.edu>
2012-10-25 01:48:50 -0500

    Pricing page

--- pricing.php
+++ pricing.php
@@ -7,10 +7,25 @@
 <h1 class="Title">Pricing</h1>
 
     <hr class="Title" />
+    <div class="mainContent">
+    
+    <p><u>For Landlords</u><br/>
+    Front-end property listing fee:&nbsp;&nbsp; $55/listing<br/>
+    Volume Listing:&nbsp;&nbsp; <a href="support.php">Click here to contact Leasehood</a></p>
 
+    <p><u>For Applicants</u><br/>
+    PFO Submittal:&nbsp;&nbsp; $6/PFO</p>
+    
+    <p><u>Discounts</u><br/>
+    LeaseHood constantly offers special rates for those parties listing rental properties  
+    in certain areas and zip codes.  If you list a property in the special area or zip code, 
+    your listing fee will be discounted 50%.  Specials typically last for one to two weeks.</p>
 
+    <p><u>Promotional Codes</u><br/>
+    If you have a promotional discount code, please make sure you enter it while listing 
+    your property or tenancy or submitting your PFO.</p>
 
-
+</div>
 <?php
 	include 'Footer.php';
 ?>
