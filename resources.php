2acfbfb584fdb363cd2cee1a04f788aa585369a3
a.long <longas@mail.lipscomb.edu>
2012-10-25 02:05:02 -0500

    Default resource page

--- resources.php
+++ resources.php
@@ -1,6 +1,28 @@
 <?php
         $title = "Resources";
 	include 'Header.php';
-	
-	include 'Footer.php';
+?>
+
+<h1 class="Title">Resources</h1>
+<hr class="Title" />
+<div class="mainContent">
+
+    <p>Throughout your experience with LeaseHood, whether an applicant or landlord, you 
+       will be provided with valuable resources to help  make your experience more enjoyable 
+       and efficient.  Once you create an account and login, you will be provided with 
+       the following resources:</p>
+    
+    <ul>
+        <li>Tips for listing property</li>
+        <li>Submitting a Proposal for Occupancy</li>
+        <li>Fair Housing laws</li>
+        <li>Tenant/landlord laws</li>
+        <li>Brokerage statutes</li>
+    </ul>
+
+
+
+</div>
+<?php
+    include 'Footer.php';
 ?>
