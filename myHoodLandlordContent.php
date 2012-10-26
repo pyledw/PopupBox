db2b7a49d7601c335aa619b1ee72e004a3367e5a
David Pyle <pyledw@gmail.com>
2012-10-26 00:05:18 -0500

    Gotten house listings to show on myHood landlord page.

--- myHoodLandlordContent.php
+++ myHoodLandlordContent.php
@@ -2,116 +2,75 @@
 <h1>My Homes</h1>
     <?php
     //pulling the data from the database and returning the PFO
-    ?>
-    <div id="myHoodListing">
+    
+    //Creates a connection to the database and stores the connection string in $con and the Selected database in $select
+    include_once 'config.inc.php';
+    $connectionInfo= get_dbconn();
+    $con = $connectionInfo[0];
+    $select = $connectionInfo[1];
+    
+    //Query to select the user's application using their userID number
+    $result = mysql_query("SELECT * FROM PROPERTY
+        WHERE UserID ='" . $_SESSION[userID] . "'");
+    if(!$result)
+    {
+        die('could not connect: ' .mysql_error());
+    }
+
+    //Setting the query results into a variable
+    while($row = mysql_fetch_array($result))
+    {
+        $ends = strtotime($row[DatePFOEndAccept]);
+        $now = strtotime(date("Y-m-d H:i:s"));
+        
+
+        $difference = $ends - $now;
+        $years = abs(floor($difference / 31536000));
+        $days = abs(floor(($difference-($years * 31536000))/86400));
+        $hours = abs(floor(($difference-($years * 31536000)-($days * 86400))/3600));
+        $mins = abs(floor(($difference-($years * 31536000)-($days * 86400)-($hours * 3600))/60));#floor($difference / 60);
+        
+        echo '    <div id="myHoodListing">
         <div class="header">
             <font class="greyTextArea" style="float:right;">Status:Show Period Closed</font>
             <font class="greyTextArea" style="float:right;">Current Rent:$1,500</font>
-            <font class="redTextArea" style="float:right;">Ended:4 hours ago</font>
+            <font class="redTextArea" style="float:right;">Ends in: ' . $years . ' Years, ' . $days . ' Days, ' . $hours . ' Hours, ' . $mins . ' Minutes</font>
         </div>
         
         <div class="content">
         <image class="PFOimage" src="#" />
         <div class="column1">
-           Address of residence<br/>
-           city<br/>
-           state<br/>
-           Zip<br/>
-           ID Number
+           '.$row[Address].'<br/>
+           '.$row[City].'<br/>
+           '.$row[State].'<br/>
+           '.$row[Zip].'<br/>
+           '.$row[PropertyID].'
         </div>
         <div class="column2">
             Bids<br/>
             Bidder ID ---  Price of Bid --- Date
+            ' .//This will be where we pull from the bids table to show the bids on the property
+            '
         </div>
         <div class="column3">
-            Details about Home
+            '.$row[Description].'
         </div>
         <div class="column4">
-            Next Open House
+            Next Open House<br/>
+            '.$row[DateTimeOpenHouse1].'<br/>
+            '.$row[DateTimeOpenHouse2].'
         </div>
         
         <div class="footer">
-            <a class="button">Review Move-in-now PFO's</a>
+            <a class="button">Review Move-in-now PFOs</a>
             <a class="button">Edit Listing</a>
-            <a class="button">Review PFO's</a>
+            <a class="button">Review PFOs</a>
             <a class="button">Print Flyer</a>
         </div>
         </div>
-    </div>
-<font class="redTextArea">
-    You have 46 hours left to take 
-    action by either accepting or rejecting this Move-in-Now 
-    PFO.  Otherwise, all applicants who submitted a PFO are 
-    relieved of their intent to lease your property.
-</font>
-<div id="myHoodListing">
-        <div class="header">
-            <font class="greyTextArea" style="float:right;">Status:Soliciting PFO's</font>
-            <font class="greyTextArea" style="float:right;">Current Rent:$1,500</font>
-            <font class="yellowTextArea" style="float:right;">Ends:4 hours and 53 Seconds</font>
-        </div>
-        
-        <div class="content">
-        <image class="PFOimage" src="#" />
-        <div class="column1">
-           Address of residence<br/>
-           city<br/>
-           state<br/>
-           Zip<br/>
-           ID Number
-        </div>
-        <div class="column2">
-            Bids<br/>
-            Bidder ID ---  Price of Bid --- Date
-        </div>
-        <div class="column3">
-            Details about Home
-        </div>
-        <div class="column4">
-            Next Open House
-        </div>
-        
-        <div class="footer">
-            <a class="button">Review Move-in-now PFO's</a>
-            <a class="button">Edit Listing</a>
-            <a class="button">Review PFO's</a>
-            <a class="button">Print Flyer</a>
-        </div>
-        </div>
-    </div>
-<div id="myHoodListing">
-        <div class="header">
-            <font class="greyTextArea" style="float:right;">Status:Pre Marketing</font>
-            <font class="greyTextArea" style="float:right;">Current Rent:$0</font>
-            <font class="greyTextArea" style="float:right;">Starts in:4 Hours</font>
-        </div>
-        
-        <div class="content">
-        <image class="PFOimage" src="#" />
-        <div class="column1">
-           Address of residence<br/>
-           city<br/>
-           state<br/>
-           Zip<br/>
-           ID Number
-        </div>
-        <div class="column2">
-            Bids<br/>
-            Bidder ID ---  Price of Bid --- Date
-        </div>
-        <div class="column3">
-            Details about Home
-        </div>
-        <div class="column4">
-            Next Open House
-        </div>
-        
-        <div class="footer">
-            <a class="button">Review Move-in-now PFO's</a>
-            <a class="button">Edit Listing</a>
-            <a class="button">Review PFO's</a>
-            <a class="button">Print Flyer</a>
-        </div>
-        </div>
-    </div>
+    </div>';
+    }
+    
+    
+    ?>
 </div>
