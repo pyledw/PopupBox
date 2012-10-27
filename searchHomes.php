1ffa16741e697f02dcb1daa40c21e67a508efd64
David Pyle <pyledw@gmail.com>
2012-10-27 11:19:21 -0500

    Beginning to work on search functionality

--- searchHomes.php
+++ searchHomes.php
@@ -10,6 +10,11 @@
 <div id="mainContent">
     <form class="formStyle" width="90%" height="90%" method="post" action="searchRedirect.php">
         Search: <input type="text" name="search">
+        <select name="type">
+            <option value="zip">Zip</option>
+            <option value="address">Address</option>
+            <option value="city">City</option>
+        </select>
         <a href="advancedHomeSearch.php">Advanced Search</a>
         <br/>
             <button type="submit" class="button">Save and Continue</button>
--- searchRedirect.php
+++ searchRedirect.php
@@ -1,16 +1,30 @@
 <?php
 
 
-            $searchTerm = $_POST['search'];
-            setcookie('search',$searchTerm);
-            header( 'Location: /searchResults.php' );
+            $type = $_POST[type];
             
+            
+            if($type == 'zip')
+            {
             //This will be used in order to allow searching via Zip code.  It will take the users inputed Zip
             //and convert it into lat and lon in order to query the locations of the properties.
             include_once 'locationLookup.php';
             $location = getLatandLongZip("37221");
             $lat = $location[0];
             $lon = $location[1];
-    
-            echo $lat . " " . $lon;
+            
+            }
+            if($type == 'address')
+            {
+                
+            }
+            if($type == 'city')
+            {
+                
+            }
+            
+            
+            $searchTerm = $_POST['search'];
+            setcookie('search',$searchTerm);
+            header( 'Location: /searchResults.php' );
 ?>
