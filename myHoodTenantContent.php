798a3e31b59a41351ed7a1bd9017dc2618e9297d
David Pyle <pyledw@gmail.com>
2012-10-28 14:05:39 -0500

    Tennant MyHood page will display all properties that you have an active bid on.

--- myHoodTenantContent.php
+++ myHoodTenantContent.php
@@ -81,7 +81,80 @@
     
     <h1>My PFOs</h1>
     <?php
+        
     //pulling the data from the database and returning the PFO
+    $result2 = mysql_query("SELECT * FROM BID
+            INNER JOIN AUCTION
+            ON AUCTION.AuctionID=BID.AuctionID
+            INNER JOIN PROPERTY
+            ON PROPERTY.PropertyID=AUCTION.PropertyID
+            INNER JOIN APPLICATION
+            ON APPLICATION.ApplicationID=BID.ApplicationID
+            WHERE APPLICATION.UserID='" . $_SESSION[userID] . "'
+            ");
+    
+    While($row2 = mysql_fetch_array($result2))
+    {
+        $ends = strtotime($row2[DatePFOEndAccept]);
+        $now = strtotime(date("Y-m-d H:i:s"));
+        
+
+        $difference = $ends - $now;
+        $years = abs(floor($difference / 31536000));
+        $days = abs(floor(($difference-($years * 31536000))/86400));
+        $hours = abs(floor(($difference-($years * 31536000)-($days * 86400))/3600));
+        $mins = abs(floor(($difference-($years * 31536000)-($days * 86400)-($hours * 3600))/60));#floor($difference / 60);
+        
+        
+        echo '    <div id="myHoodListing">
+        <div class="header">
+            <font class="greyTextArea" style="float:right;">Status:Show Period Closed</font>
+            <font class="greyTextArea" style="float:right;">Current Rent:$1,500</font>
+            <font class="redTextArea" style="float:right;">Ends in: ' . $years . ' Years, ' . $days . ' Days, ' . $hours . ' Hours, ' . $mins . ' Minutes</font>
+        </div>
+        
+        <div class="content">
+        <image class="PFOimage" src="#" />
+        <div class="column1">
+           '.$row2[Address].'<br/>
+           '.$row2[City].'<br/>
+           '.$row2[State].'<br/>
+           '.$row2[Zip].'<br/>
+           '.$row2[PropertyID].'
+        </div>
+        <div class="column2">
+            Bids<br/>
+            Bidder ID ---  Price of Bid --- Date
+            ' .//This will be where we pull from the bids table to show the bids on the property
+            '
+        </div>
+        <div class="column3">
+            '.$row2[Description].'
+        </div>
+        <div class="column4">
+            Next Open House<br/>
+            '.$row2[DateTimeOpenHouse1].'<br/>
+            '.$row2[DateTimeOpenHouse2].'
+        </div>
+        
+        <div class="footer">
+        <form class="buttonForm" method="post" action="newListing1.php">
+            <input type="text" name="propertyID" style="Display:none" value="' . $row2[PropertyID] . '" />
+            <button class="button">Edit Listing</button>
+        </form>
+        <form class="buttonForm" method="post" action="reviewPFOs.php">
+            <input type="text" name="propertyID" style="Display:none" value="' . $row2[PropertyID] . '" />
+            <button class="button">Review PFOs</button>
+        </form>
+        <form class="buttonForm" method="post" action="printFlyer.php">
+            <input type="text" name="propertyID" style="Display:none" value="' . $row2[PropertyID] . '" />
+            <button class="button">Print Flyer</button>
+        </form>
+        </div>
+        </div>
+    </div>';
+    }
+    
     ?>
     <div id="myHoodListing">
         <div class="header">
