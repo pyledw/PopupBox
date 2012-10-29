13957f3b474cdaad2a27decb78a92ed370b9a0b1
Jason Dickinson <yodaj007@gmail.com>
2012-10-29 17:19:05 -0500

    Logins need to use the hashed password

--- loginRedirect.php
+++ loginRedirect.php
@@ -9,45 +9,27 @@
         $con = get_dbconn();
 
         //Query the database for only the row containing that users information
-        $result = mysql_query("SELECT * FROM USER
-            WHERE UserName ='" . $myName . "'");
+        $result = mysql_query("SELECT * FROM USER WHERE UserName ='" . $myName . "' AND PASSWORD = '" . crypt($userPassword) . "'");
         if(!$result)
         {
             die('could not connect: ' .mysql_error());
         }
         
         //fetching the array of query elements
-        while($row = mysql_fetch_array($result))
+        if (($row = mysql_fetch_array($result)) != NULL)
         {
-            //echo $row['Password'];
             $userType = $row['AccountType'];
             $userID = $row['UserID'];
-            //Testing to make sure it is a valid name
-            //This will be where we check to see if credentails are correct
-            
-            if($userPassword != $row['Password'])
-            {
-                
-                echo 'Incorrect Password';
-            }
-            //if credentials are correct this will set the  so the session will
+
+            //this will set the  so the session will
             //be remembered.  Might also eventually use a session to store some of this data
             //as it is more secure.
             //It then redirects the user to the myHood page.
-            else
-            {
-                $_SESSION['userID'] = $userID;
-                $_SESSION['user'] = $myName;
-                $_SESSION['type'] = $userType;
-                header( 'Location: /myHood.php' );
-            }
+            $_SESSION['userID'] = $userID;
+            $_SESSION['user'] = $myName;
+            $_SESSION['type'] = $userType;
+            header( 'Location: /myHood.php' );
         }
-        
+
         echo "Username does not exist";
-        
-        
-        
-        
-        
-    
 ?>
