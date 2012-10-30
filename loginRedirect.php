5cd4d765a79faa712fb142e60ce75378e48aaa6f
David Pyle <pyledw@gmail.com>
2012-10-29 22:39:15 -0500

    Still trying to figure out login... It is not working... for any login...

--- loginRedirect.php
+++ loginRedirect.php
@@ -9,17 +9,18 @@
         $con = get_dbconn("");
 
         //Query the database for only the row containing that users information
-        //$result = mysql_query("SELECT * FROM USER WHERE UserName ='" . $myName . "' AND PASSWORD = '" . crypt($userPassword) . "'");
+        $result = mysql_query("SELECT * FROM USER WHERE UserName ='" . $myName . "' AND PASSWORD = '" . crypt($userPassword) . "'");
         
         
         //Changed to allow login for testing
-        $result = mysql_query("SELECT * FROM USER WHERE UserName ='" . $myName . "' AND PASSWORD = '" . $userPassword . "'");
+        //$result = mysql_query("SELECT * FROM USER WHERE UserName ='" . $myName . "' AND PASSWORD = '" . $userPassword . "'");
   
         
         if(!$result)
         {
             die('could not connect: ' .mysql_error());
         }
+        
         
         //fetching the array of query elements
         if (($row = mysql_fetch_array($result)) != NULL)
