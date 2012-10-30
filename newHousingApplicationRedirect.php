b2fd8698d2c57d6535b873ca0ae75861fb65f262
David Pyle <pyledw@gmail.com>
2012-10-29 20:46:51 -0500

    Working on changing DB connections for all forms

--- newHousingApplicationRedirect.php
+++ newHousingApplicationRedirect.php
@@ -37,12 +37,11 @@ session_start();
     try {
         $stmt->bindValue(':numbOccupants',  $_POST['numbOccupants'],		PDO::PARAM_INT);
         $stmt->bindValue(':fName', 	    $_POST['fName'],                    PDO::PARAM_STR);
-        $stmt->bindValue(':userID',         $_SESSION['userID'],                   PDO::PARAM_STR);
+        $stmt->bindValue(':userID',         $_SESSION['userID'],                PDO::PARAM_STR);
         $stmt->bindValue(':earliestDate',   $_POST['email1'],                   PDO::PARAM_STR);
         $stmt->bindValue(':latestDate',     $_POST['latestDate'],               PDO::PARAM_STR);
         $stmt->bindValue(':ADA',            $_POST['ADA'],                      PDO::PARAM_INT);
         $stmt->bindValue(':smoking',        $_POST['smoking'],                  PDO::PARAM_INT);
-        $stmt->bindValue(':lName',          $_POST['lName'],                    PDO::PARAM_STR);
         $stmt->bindValue(':age',            $_POST['age'],                      PDO::PARAM_INT);
         $stmt->bindValue(':relationship',   $_POST['relationship'],             PDO::PARAM_STR);
         $stmt->bindValue(':animalType',     $_POST['animalType'],               PDO::PARAM_STR);
@@ -83,6 +82,54 @@ session_start();
      */
     else
     {
+        
+        //Using the new method for inserting into the Database
+    $con = get_dbconn("PDO");
+    $stmt = $con->prepare("
+            UPDATE APPLICATION SET
+                NumOtherOccupants=:numbOccupants,        SecondaryOccupantFName=:fName,                UserID=:userID,                    EarlyMoveIn=:earliestDate,
+                LateMoveIn=:latestDate,               IsADA=:ADA,                                 IsSmokingRequired=:smoking,         SecondaryOccupantAge=:age, 
+                SecondaryOccupantRelationship=:relationship,                                   Pet1Type=:animalType,                  Pet1Breed=:animalBreed,
+                Pet1Age=:animalAge,                  Pet1Weight=:animalWeight,                            Pet2Type=:animalType2,                  Pet2Breed=:animalBreed2,
+                Pet2Age=:animalAge2,                  Pet2Weight=:animalWeight2,                            Pet3Type=:animalType3,                  Pet3Breed=:animalBreed3,
+                Pet3Age=:animalAge3,                  Pet3Weight=:animalWeight3,                            SecondaryOccupantLName=:lName
+                
+            WHERE UserID='$_SESSION[userID]'
+            ");
+    try {
+        $stmt->bindValue(':numbOccupants',  $_POST['numbOccupants'],		PDO::PARAM_INT);
+        $stmt->bindValue(':fName', 	    $_POST['fName'],                    PDO::PARAM_STR);
+        $stmt->bindValue(':userID',         $_SESSION['userID'],                PDO::PARAM_STR);
+        $stmt->bindValue(':earliestDate',   $_POST['earliestDate'],             PDO::PARAM_STR);
+        $stmt->bindValue(':latestDate',     $_POST['latestDate'],               PDO::PARAM_STR);
+        $stmt->bindValue(':ADA',            $_POST['ADA'],                      PDO::PARAM_INT);
+        $stmt->bindValue(':smoking',        $_POST['smoking'],                  PDO::PARAM_INT);
+        $stmt->bindValue(':age',            $_POST['age'],                      PDO::PARAM_INT);
+        $stmt->bindValue(':relationship',   $_POST['relationship'],             PDO::PARAM_STR);
+        $stmt->bindValue(':animalType',     $_POST['animalType'],               PDO::PARAM_STR);
+        $stmt->bindValue(':animalBreed',    $_POST['animalBreed'],              PDO::PARAM_STR);
+        $stmt->bindValue(':animalAge',      $_POST['animalAge'],                PDO::PARAM_INT);
+        $stmt->bindValue(':animalWeight',   $_POST['animalWeight'],             PDO::PARAM_INT);
+        $stmt->bindValue(':animalType2',    $_POST['animalType2'],              PDO::PARAM_STR);
+        $stmt->bindValue(':animalBreed2',   $_POST['animalBreed2'],             PDO::PARAM_STR);
+        $stmt->bindValue(':animalAge2',     $_POST['animalAge2'],               PDO::PARAM_INT);
+        $stmt->bindValue(':animalWeight2',  $_POST['animalWeight2'],            PDO::PARAM_INT);
+        $stmt->bindValue(':animalType3',    $_POST['animalType3'],              PDO::PARAM_STR);
+        $stmt->bindValue(':animalBreed3',   $_POST['animalBreed3'],             PDO::PARAM_STR);
+        $stmt->bindValue(':animalAge3',     $_POST['animalAge3'],               PDO::PARAM_INT);
+        $stmt->bindValue(':animalWeight3',  $_POST['animalWeight3'],            PDO::PARAM_INT);
+        $stmt->bindValue(':lName',          $_POST['lName'],                    PDO::PARAM_STR);
+        $stmt->execute();
+    } catch (Exception $e) {
+	echo 'Connection failed. ' . $e->getMessage();
+    }
+
+    echo "1 record added";
+    
+    
+    /*
+     *
+     
     
         mysql_query("UPDATE APPLICATION SET NumOtherOccupants='$_POST[numbOccupants]'
             WHERE UserID = '$_SESSION[userID]'");
@@ -158,6 +205,8 @@ session_start();
         
         mysql_query("UPDATE APPLICATION SET Pet4Weight='$_POST[animalWeight4]'
             WHERE UserID = '$_SESSION[userID]'");
+     * 
+     */
     }
     
     
