f4620d942bde05a67001cb08cca9f08ece21bab3
a.long <longas@mail.lipscomb.edu>
2012-11-04 00:07:28 -0500

    filling in content

--- resourcesLandlordContent.php
+++ resourcesLandlordContent.php
@@ -1,18 +1,34 @@
 <?php
-
+        $title = "Resources - Landlord";
+	include 'Header.php';
 
 ?>
 
-<h1 class="Title">Landlord Resources</h1>
+    <h1 class="Title">Landlord Resources</h1><br />
     <hr class="Title" />
     <div id="mainContent">
-    <?php
-    //pulling the data from the database and returning the PFO
+
+<?php
+    //pulling the data from the database
     
-    //Creates a connection to the database and stores the connection string in $con and the Selected database in $select
     include_once 'config.inc.php';
     $connectionInfo= get_dbconn();
     $con = $connectionInfo[0];
     $select = $connectionInfo[1];
     
-    
+?>
+        <h3><a id="top">LeaseHood offers the following resources to maximize your position as a landlord:</a></h3><br />
+        
+        <p><font color="blue" ><b>Tips for listing property:</b></font><br />
+        These tips will help enhance your position as a Landlord and maximize your potential while using LeseHood.com.<br />
+        From open house tips to listing tips to pricing tips, it’s all here.</p>
+
+        <p><font color="blue" weight="bold">TN Tenant/Landlord Rights:</font><br />
+        As a landlord, you have both obligations and rights regarding your tenants, and it is very important to <br/>
+        understand the Landlord Tenant Act.</p>
+        
+        <p><font color="blue" weight="bold">Fair Housing laws:</font><br/>
+        Fair Housing Laws are established to eliminate discrimination between otherwise identical parties.  As a <br/>
+        Landlord, you are strongly encouraged to abide by them.</p>
+        
+        
