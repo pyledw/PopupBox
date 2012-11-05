38aa6955387a9a8d49e848f55088c9cce8871b0d
Caleb <Caleb@Plato.lipscomb.edu>
2012-11-04 20:28:54 -0600

    added contact.php link in footer and created contact.php page

---
+++ contact.php
@@ -0,0 +1,15 @@
+<?php
+        $title = "Welcome";
+	include 'Header.php';
+?>
+    <h1 class="Title">Contact LeaseHood</h1>
+        <hr class="Title" />
+        <h2 class="Title">Email <a href="mailto:a@a.com> LeaseHood</a>
+</h2>
+
+
+
+<?php
+	include 'Footer.php';
+?>
+
