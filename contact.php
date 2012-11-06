3538743cea904cb40dbbc3455bfed49b80c7af32
Caleb <Caleb@Plato.lipscomb.edu>
2012-11-05 20:23:55 -0600

    created affiliations page, added link in footer

--- contact.php
+++ contact.php
@@ -1,5 +1,5 @@
 <?php
-        $title = "Welcome";
+        $title = "Contact Us";
 	include 'Header.php';
 ?>
     <h1 class="Title">Contact LeaseHood</h1>
@@ -7,32 +7,33 @@
         <h2 class="Title">Email <a href="mailto:a@a.com"> LeaseHood</a></h2>
 <div id="mainContent">
             <table width="1000px">
-                <tr style="height: 25px;">
+                <tr style="height: 20px;">
                     <td>
-                        LeaseHood, LLC
+                        <b>LeaseHood, LLC</b>
                     </td>
                 </tr>
-                <tr style="height: 25px;">
+                <tr style="height: 20px;">
                     <td>
                         P.O. Box 158479
                     </td>
                 </tr>
-                <tr style="height: 25px;">
+                <tr style="height: 20px;">
                     <td>
                         Nashville, Tennessee 37215
                     </td>
                 </tr>
-                <tr style="height: 25px;">
+                <tr style="height: 20px;">
                     <td>
                         P: (615) 202-0549 (M-F, 8 am to 5 pm, CST)
                     </td>
                 </tr>
-                <tr style="height: 25px;">
+                <tr style="height: 20px;">
                     <td>
                         F: (866) 357-1018
                     </td>
                 </tr>
             </table>
+    <br>
 
 <?php
 	include 'Footer.php';
