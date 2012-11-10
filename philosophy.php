8a3e560ad8e888c74bb3706a5b8911c8d715dd43
Caleb <Caleb@Plato.lipscomb.edu>
2012-11-10 16:04:05 -0600

    formatting - philosophy and mission

--- philosophy.php
+++ philosophy.php
@@ -5,7 +5,7 @@
 	include 'Header.php';
 ?>
 
-<h1 class="Title">Philosophy and Missions</h1>
+<h1 class="Title">Philosophy and Mission</h1>
 <hr class="Title" />
 <div id="mainContent">
     <p>It is the goal of LeaseHood.com to offer the most innovative and efficient approach of introducing available residential rental property to prospective residents.  LeaseHood.com documents its decision to fully comply with all rules, statutes, and ethical standards relative to the operation of a real estate brokerage and real estate advertising company as well as those work practices required by the National Association of Realtors and Civil Rights Act of 1968 (federal Fair Housing Act) and its appropriate amendments.</p>
