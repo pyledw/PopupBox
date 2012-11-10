7e058c28273c6bc8fbd9df13c7399209e7ef2b62
Caleb <Caleb@Plato.lipscomb.edu>
2012-11-10 15:34:36 -0600

    filled in content for tips for success

--- tipsForSuccess.php
+++ tipsForSuccess.php
@@ -5,12 +5,31 @@
 	include 'Header.php';
 ?>
 
-<h1 class="Title">Tips for Success</h1>
-<div id="maincontent">
-<p>
+<h1 class="Title">Tips for Success</h1><h2> (Landlords)</h2>
+<hr class="Title" />
+<div id="mainContent">
+    <p><u>Be completely honest:</u> This applies to your downloaded photos, property descriptions, and rental expectations.  Consumers are savvy and will be able to clearly identify alternative agendas.  Consumers are more educated and savvy than ever before.  When you don't portray a representative photo of your property’s kitchen, you’re only wasting your time and possibly other’s time.  Consumers want to know how every important aspect of your property looks, without the need to visit your property to find out.  Many consumers like kitchens and the exterior of their home; it identifies them.  If you have a substandard property, it should be reflected in your rental price. </p>
+
+    <p><u>Pricing:</u> Consider the economy and the supply and demand principle.  Typically, when the economy is down,  foreclosures are up, unemployment rates are high, or lending restrictions are tight, there are fewer home buyers.  Homes sales are often inversely proportionate to rental rates.  Fewer home buyers mean more renters.  Assuming the number of rental homes (supply) available remains relatively unchanged, there becomes a disproportionate number of seeking renters (demand).  When demand exceeds supply, prices increase.  So, when the economy is down, often times a landlord’s market is realized and rental rates are higher.  The inverse is true as well.  Rental rates fluctuate in any given year, and sometimes even by month.  LeaseHood’s primary purpose for landlords is to capture the highest rental rate at any given time, regardless of economic conditions. </p>
+
+    <p>As with houses for sale, the single biggest deterrent of a rental property is likely an inappropriate rental price.  Therefore, it is important that you price your property in an educated  and realistic manner.  Recent lease agreements and online resources are often used to determine the rental value of a home.  Once you have established a perceived fair market value, your starting price should be below this value.  A good rule of thumb is for the starting price to be 80-90% of the market value because you will need to present a starting price for you property that creates interest.  Similarly, you should select a “Move-in-Now” price slightly above market value.  But be careful.  An unrealistic “Move-in-Now” price will likely deter otherwise interested parties.  Also be mindful that any additional rental rate you receive over your perceived market value will likely yield additional income for the duration of the lease, and often times, beyond.  An extra $50 per month is $600 per year!  But be careful to not be too greedy.</p>
+    
+    <p><u>Curb Appeal:</u>Make sure your property is tidy and you maximize the curb appeal.  Many prospective renters assume a house’s inside features are no more attractive than the exterior appearance.   You want to generate interest from street traffic and make them want to read your flyer and visit the open house.  During your home marketing campaign, ensure that the grass height is maintained, flower beds are cleaned, trash is picked up, etc.</p>
+
+    <p><u>The Open House and Show Period:</u>The way a house looks, smells, and sounds are very important.  It is essential that you create an atmosphere to allow the perspective tenant to imagine himself or herself living in your home.  Remove family photos if a tenant is living in the house during show times.  Clean the carpet, wax the floors, patch the walls, clean the closets.   Since the kitchen and bathroom are often the first rooms inspected, ensure they are spotless.  Eliminate any offensive odors and use an air freshener (but don’t overdo it).  Play some smooth jazz, classical, or other inviting music in the background.</p>
+
+    <p>For best results, you need to conduct at least one open house.  As far as when to conduct your open house(s), there are several alternatives, depending on for how long you intend to make the property available for PFOs.
+        <br/>(1) If you have marketed your property as a “Coming Soon” property for 7-10 days and intend to solicit PFOs for a short period of time, say 7 days or less, you may want to conduct an open house within a few days or so of making your property available for rent on LeaseHood.com.   If you receive several messages from prospective applicants requesting an additional open house, you may want to schedule an additional open house on or near the last day of the show window.
+	<br/>(2) If you have marketed your property as a “Coming Soon” property for 7-10 days and intend to solicit PFOs for a longer period of time, say 7-14 days, you may want to conduct an open house within a few days or so of making your property available for rent on LeaseHood.com and an additional open house at or near the end of the show window.
+	<br/>(3) If you have not marketed your property as a “Coming Soon” property, you may want to consider making your show window 10-14 days.  This way, you will allow more time for interested parties to hit the market.  It is recommended you conduct an open house within 4-7 days after making to property available on LeaseHood.com and an additional open house at or near the end of the show window.</p>
+    
+    <p>Several hundred NEW prospective home renters (not apartment seekers) typically enter the market every 14 days in a metropolitan area of 1 million or more.  So, it is important to expose your property to the most people for the longest duration.  Please use the following  table to assist in selecting your pre-marketing, show window duration and open house parameters:</p>
+
+<img src="images/days_table.jpg" alt="Days table" />
+
     
 
-</p>
+
 </div>
 
 
