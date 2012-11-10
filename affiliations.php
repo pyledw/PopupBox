dd7cb1f28420c450033df0db0c6f625827622119
Caleb <Caleb@Plato.lipscomb.edu>
2012-11-10 14:56:42 -0600

    filled out content for affiliations and licensing

--- affiliations.php
+++ affiliations.php
@@ -5,7 +5,18 @@
 <h1 class="Title">Affiliations & Licensing</h1>
         <hr class="Title" />
         
-        <br>
+        <p><u>Affiliations</u><br/>
+        LeaseHood is honored to be members of several organizations that help integrate itself with the real estate industry and local business development; we believe involvement helps foster our desire to cooperate with other liked-minded parties.</p>
+
+<p>As a member of the Greater Nashville Association of Realtors®, LeaseHood embraces other professionals within the real estate industry and commits to conduct itself ethically and respectfully to all parties involved.  Essentially, what separates a Realtor® from a non-Realtor® is the commitment to a higher calling by complying with a comprehensive code of ethics.  If for some reason you feel LeaseHood has conducted itself unethically during your experience, please contact us here.  </p>
+
+<p>As a member of the local Chamber of Commerce, LeaseHood involves itself with the leadership and economic prosperity of fellow businesses within the community.</p> 
+
+<p><u>Licensing</u><br/>
+LeaseHood maintains a Real Estate Broker license within the state of Tennessee.  "Brokerage" and "Agency" typiclly refer to the compensation for the introducton of two opposing parties for the execution of a real estate transaction .  Since all properties are listed with an up-front fee regardless of whether a transaction occurs, such listing fee is considered a listing fee only and not compensation and therefore not brokerage.  However, LeaseHood, LLC maintains a Real Estate Broker License and is part of the local chapter of the National Association of Realtors.</p> 
+
+        
+        <br/>
         
 <?php
 	include 'Footer.php';
