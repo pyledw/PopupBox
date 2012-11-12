479807f7930b83dcbc0cfb591f98a39d40492d80
David Pyle <pyledw@gmail.com>
2012-11-11 20:01:14 -0600

    test

--- processComments.php
+++ processComments.php
@@ -3,12 +3,25 @@
         $title = "Comments Submmitted";
 	include 'Header.php';
 ?>
-<h1 class="Title">Comments Submitted</h1>
-    <hr class="Title" />
-    <div id="mainContent">
-        
-        
-    </div>
+<?php
+   $to = "pyledw@gmail.com";
+   $subject = "This is subject";
+   $message = "<b>This is HTML message.</b>";
+   $message .= "<h1>This is headline.</h1>";
+   $header = "From:abc@somedomain.com \r\n";
+   $header = "Cc:afgh@somedomain.com \r\n";
+   $header .= "MIME-Version: 1.0\r\n";
+   $header .= "Content-type: text/html\r\n";
+   $retval = mail ($to,$subject,$message,$header);
+   if( $retval == true )
+   {
+      echo "Message sent successfully...";
+   }
+   else
+   {
+      echo "Message could not be sent...";
+   }
+?>
     
 <?php
     include 'Footer.php';
