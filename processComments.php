08c8433aa5545fd605c6aeddc139ec0bf6808678
a.long <longas@mail.lipscomb.edu>
2012-11-12 10:16:49 -0600

    adding email function

--- processComments.php
+++ processComments.php
@@ -2,11 +2,15 @@
 
         $title = "Comments Submmitted";
 	include 'Header.php';
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $email = $_POST['email'];
+        $comments = $_POST['comments'];
 ?>
 <h1 class="Title">Comments Submitted</h1>
     <hr class="Title" />
     <div id="mainContent">
-        
+        <p>Thank you for your feedback. You will receive a reply from our support staff between 1-3 business days.</p>
         
     </div>
     
