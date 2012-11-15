9254abf137098c89dc3c49bb1c665ecb3403621c
David Pyle <pyledw@gmail.com>
2012-11-15 13:28:24 -0600

    Added search to the search results page.

--- searchResults.php
+++ searchResults.php
@@ -19,10 +19,30 @@
         
         
 ?>
+<form style="float:right;" method="post" action="searchRedirect.php">
+        <table width="" style="text-align: center;">
+        <tr>
+            <td>
+                <label class="label">Search: </label><input type="text" value="<?php echo $val; ?>" name="search">
+        <select name="type">
+                <option <?php if($type == "zip"){echo 'selected="selected"';} ?> value="zip">Zip</option>
+                <option <?php if($type == "address"){echo 'selected="selected"';} ?> value="address">Address</option>
+                <option <?php if($type == "city"){echo 'selected="selected"';} ?> value="city">City</option>
+        </select>
+                 
+            </td>
+            <td>
+                <button type="submit" class="button">Search</button>
+            </td>
+        </tr>
+        </table>
+    </form>
 <h1 class="Title">Home Search Results</h1>
+
 <hr class="Title" />
+
 <div id="mainContent">
-    
+
  <link rel="stylesheet" type="text/css" href="css/homeListing.css" /><!--Link to Main css file -->
     <div id="mainContent" >
             
