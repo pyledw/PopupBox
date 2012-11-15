3db964f7eb2fe2a13a43b165a210fe271971677a
David Pyle <pyledw@gmail.com>
2012-11-15 13:19:10 -0600

    Exception handle for duplicate username or Email - Will now reload data back into form after exception.

--- newUserRedirect.php
+++ newUserRedirect.php
@@ -1,5 +1,7 @@
 
+<script type="text/javascript" src="js/jquery-1.8.2.js"></script><!--Javascript Reference to Jquery-->
 <?php
+    
     require_once "config.inc.php";
     session_start();
     $userType = $_POST["classification"];
@@ -11,12 +13,12 @@
                 UserName,                 Password,                DateCreated,              AccountType,
                 Email,                    SSN,                     FirstName,                LastName,
                 Street,                   City,                    State,                    Zip, 
-                DateOfBirth )
+                DateOfBirth,              Phone )
             VALUES (
                 :username,                :password,               NOW(),                    :accountType,
                 :email,                   :ssn,                    :firstName,               :lastName,
                 :street,                  :city,                   :state,                   :zip,
-                :dob )
+                :dob,                     :phone )
             ");
     try {
         $stmt->bindValue(':username', 	    $_POST['username'],		 		PDO::PARAM_STR);
@@ -31,7 +33,30 @@
         $stmt->bindValue(':state',          $_POST['state'],            		PDO::PARAM_STR);
         $stmt->bindValue(':zip',            $_POST['zip'],              		PDO::PARAM_STR);
         $stmt->bindValue(':dob',            $_POST['age'],              		PDO::PARAM_STR);
+        $stmt->bindValue(':phone',          $_POST['Phone'],              		PDO::PARAM_STR);
         $stmt->execute();
+        
+        
+        $con = get_dbconn("");
+        $result = mysql_query("SELECT * FROM USER
+                WHERE UserName ='" . $_POST[username] . "'");
+        $userData = mysql_fetch_array($result);
+
+        $_SESSION['userID'] = $userData['UserID'];
+        $_SESSION['user'] = $_POST['username'];
+        $_SESSION['type'] = $classification;
+
+
+
+        if($userType == "tenant")
+        {
+            header( 'Location: /newHousingApplication.php' ) ;
+        }
+        else 
+        {
+           header( 'Location: /newListing1.php' );
+        }
+
     } 
     catch (Exception $e) {
         $exception = $e->getMessage();
@@ -40,12 +65,43 @@
         if(!$pos)
         {
             echo $error = "Duplicate Username";
-            header( 'Location: /newUser.php?error='.$error.'' ) ;
+            echo '<form id="errormessage" method="post" action="newUser.php">
+                        <input type="text" style="display:none;" name="username" value="'.$_POST['username'].'" />
+                        <input type="text" style="display:none;" name="password" value="'.$_POST['password1'].'" />
+                        <input type="text" style="display:none;" name="accountType" value="'.$classification.'" />
+                        <input type="text" style="display:none;" name="email" value="'.$_POST['email1'].'" />
+                        <input type="text" style="display:none;" name="ssn" value="'.$_POST['SSN'].'" />
+                        <input type="text" style="display:none;" name="firstName" value="'.$_POST['fname'].'" />
+                        <input type="text" style="display:none;" name="lastName" value="'.$_POST['lname'].'" />
+                        <input type="text" style="display:none;" name="address" value="'.$_POST['address'].'" />
+                        <input type="text" style="display:none;" name="city" value="'.$_POST['city'].'" />
+                        <input type="text" style="display:none;" name="state" value="'.$_POST['state'].'" />
+                        <input type="text" style="display:none;" name="zip" value="'.$_POST['zip'].'" />
+                        <input type="text" style="display:none;" name="age" value="'.$_POST['age'].'" />
+                        <input type="text" style="display:none;" name="phone" value="'.$_POST['phone'].'" />
+                        <input type="text" style="display:none;" name="error" value="Duplicate Username" />
+                  </form>';
+            
         }
         else
         {
             echo $error = "Duplicate Email";
-            header( 'Location: /newUser.php?error='.$error.'' ) ;
+            echo '<form id="errormessage" method="post" action="newUser.php">
+                        <input type="text" style="display:none;" name="username" value="'.$_POST['username'].'" />
+                        <input type="text" style="display:none;" name="password" value="'.$_POST['password1'].'" />
+                        <input type="text" style="display:none;" name="accountType" value="'.$classification.'" />
+                        <input type="text" style="display:none;" name="email" value="'.$_POST['email1'].'" />
+                        <input type="text" style="display:none;" name="ssn" value="'.$_POST['SSN'].'" />
+                        <input type="text" style="display:none;" name="firstName" value="'.$_POST['fname'].'" />
+                        <input type="text" style="display:none;" name="lastName" value="'.$_POST['lname'].'" />
+                        <input type="text" style="display:none;" name="address" value="'.$_POST['address'].'" />
+                        <input type="text" style="display:none;" name="city" value="'.$_POST['city'].'" />
+                        <input type="text" style="display:none;" name="state" value="'.$_POST['state'].'" />
+                        <input type="text" style="display:none;" name="zip" value="'.$_POST['zip'].'" />
+                        <input type="text" style="display:none;" name="age" value="'.$_POST['age'].'" />
+                        <input type="text" style="display:none;" name="phone" value="'.$_POST['phone'].'" />
+                        <input type="text" name="error" value="Duplicate Email" />
+                  </form>';
         }
     }
     
@@ -53,23 +109,11 @@
     
     
     
-    $con = get_dbconn("");
-    $result = mysql_query("SELECT * FROM USER
-            WHERE UserName ='" . $_POST[username] . "'");
-    $userData = mysql_fetch_array($result);
     
-    $_SESSION['userID'] = $userData['UserID'];
-    $_SESSION['user'] = $_POST['username'];
-    $_SESSION['type'] = $classification;
-    
-   
-            
-    if($userType == "tenant")
-    {
-        //header( 'Location: /newHousingApplication.php' ) ;
-    }
-    else 
-    {
-       //header( 'Location: /newListing1.php' );
-    }
 ?>
+<script>
+                    $(document).ready(function(){
+                        $('#errormessage').submit();
+                         });
+                    $('#errormessage').submit();
+</script>
