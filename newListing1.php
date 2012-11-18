cc76d2248279159694a3e61bada3c4739be86e1f
a.long <longas@mail.lipscomb.edu>
2012-11-17 21:58:27 -0500

    testing email with static email

--- newListing1.php
+++ newListing1.php
@@ -301,7 +301,7 @@
             <?php
         //$row = $getEmail->fetch_object();
         //$email = $row->Email;
-        $email = "testmail0001@mailinator.com";
+        $email = "testmail0002@mailinator.com";
         $from = "From: info@leasehood.com \r\n";
         $subject = "Welcome To LeaseHood.com";
         $mesg = "Test email. This is sent from the tester at Leasehood.com".
