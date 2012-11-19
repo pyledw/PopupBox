7170adec1c01efea119fadd2ee3fd43375bd3d56
David Pyle <pyledw@gmail.com>
2012-11-19 13:18:35 -0600

    Fix for redirect on myhood page.

--- myHood.php
+++ myHood.php
@@ -11,7 +11,7 @@
          */
         session_start();
         if(!isset($_SESSION[userID])){
-           // header( 'Location: /login.php' );
+           header( 'Location: /login.php' );
         }
         $title = "MyHood - Home";
 	include "Header.php";
