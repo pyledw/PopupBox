79be878f44a8b79d1a96e48d55bf420a0df5fdad
David Pyle <pyledw@gmail.com>
2012-11-19 20:57:43 -0600

    IMAGE UPLOADS WORKING!

--- imageUpload.php
+++ imageUpload.php
@@ -5,19 +5,6 @@ $result = array();
 if (count($_FILES) > 0)
 {
 	$result = handle_uploads(4, $_FILES);
-	echo "<pre>This is the return value from handle_uploads:";
-	print_r($result);
-	echo "</pre>";
-	echo "<br /><br />";
-	echo "THIS DEMONSTRATES HOW TO USE THE RETURN VALUE TO GENERATE IMG TAGS <br />";
-	foreach ($result as $upload)
-	{
-		if (!$upload[error]) 
-		{
-	?>
-			<img src='<?= $upload[name_thumb]; ?>'></img>
-	<?
-		}
-	}
+	echo 'IMAGE IS UPLOADING';
 }
 ?>
--- newListing2.php
+++ newListing2.php
@@ -72,7 +72,9 @@
                     <td>
                         <form action="imageUpload.php" method="post" enctype="multipart/form-data">
 			<label for="file">File #1</label>
+                        
 			<input type="file" name="file" id="file" />
+                        <input type="text" name="propertyID" style="display: none;" value='<?php echo $propertyID; ?>' />
 			<br />
 			<input type="submit" name="submit" value="Submit" />
                         </form>
