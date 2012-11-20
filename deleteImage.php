c9412a8b275b84fb3bfabf3d8502ac4500bbe27c
David Pyle <pyledw@gmail.com>
2012-11-20 15:42:16 -0600

    setup for deleting an image.

--- deleteImage.php
+++ deleteImage.php
@@ -8,5 +8,37 @@
     
     echo $_GET[imageID];
     
+    
+    $db_user 	= 'leasehood';
+    $db_pass 	= 'sunlight blanket floating change';  // db password
+    $db_server 	= '199.115.231.216';     // server name or IP address
+    $db_database = 'leasehood1112';     // database to select
+    
+    $con = mysql_connect($db_server,$db_user,$db_pass );
+        if(!$con)
+        {
+            die('could not connect: ' .mysql_error());
+        }
+        else
+        {
+            //echo "connected to mySQL";
+        }
+
+        $select = mysql_selectdb($db_database, $con);
+
+        if(!$select)
+        {
+            die('could not connect: ' .mysql_error());
+        }
+        else
+        {
+            //echo "Selected Database";
+        }
+        
+        mysql_query("DELETE * FROM IMAGE
+            WHERE ImageID=$_GET[imageID]");
+        
+        
+        
     echo 'This will eventually be a function to delete the listing.'; 
 ?>
