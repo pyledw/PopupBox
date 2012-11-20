5538b1a2a35a9e0bdf9197506b9edaa05c8d65ed
David Pyle <pyledw@gmail.com>
2012-11-19 22:50:56 -0600

    I hate COMMITS!

---
+++ deleteImage.php
@@ -0,0 +1,12 @@
+<?php
+
+    /**
+     * This page will delete the selected image from the server and database.
+     * 
+     */
+    $_GET[imageID];
+    
+    echo $_GET[imageID];
+    
+    echo 'This will eventually be a function to delete the listing.'; 
+?>
