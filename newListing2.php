abbbde44322cead662826666932273b6da9dfd45
David Pyle <pyledw@gmail.com>
2012-11-19 21:15:10 -0600

    You can now upload Images - one at a time.  They go slow, but they work.

--- newListing2.php
+++ newListing2.php
@@ -49,34 +49,6 @@
     
     
 ?>
-    <script src="http://malsup.github.com/jquery.form.js"></script>
-    <script>
-    (function() {
-
-    var bar = $('.bar');
-    var percent = $('.percent');
-    var status = $('#status');
-
-    $('form').ajaxForm({
-        beforeSend: function() {
-            status.empty();
-            var percentVal = '0%';
-            bar.width(percentVal)
-            percent.html(percentVal);
-        },
-        uploadProgress: function(event, position, total, percentComplete) {
-            var percentVal = percentComplete + '%';
-            bar.width(percentVal)
-            percent.html(percentVal);
-                    //console.log(percentVal, position, total);
-        },
-            complete: function(xhr) {
-                    status.html(xhr.responseText);
-            }
-    }); 
-
-    })();       
-</script>
     <h1 class="Title">New House Listing - Photos</h1>
     <hr class="Title">
     <div id="mainContent">
