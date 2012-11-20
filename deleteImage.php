63170bb43afd28b390b20cfa0b9996aff4125c1f
David Pyle <pyledw@gmail.com>
2012-11-20 15:53:53 -0600

    Trying to delete images...

--- deleteImage.php
+++ deleteImage.php
@@ -8,49 +8,27 @@
     
     echo $_GET[imageID];
     
+    include 'config.inc.php';
     
-    $db_user 	= 'leasehood';
-    $db_pass 	= 'sunlight blanket floating change';  // db password
-    $db_server 	= '199.115.231.216';     // server name or IP address
-    $db_database = 'leasehood1112';     // database to select
+    $con = get_dbconn("");
     
-    $con = mysql_connect($db_server,$db_user,$db_pass );
-        if(!$con)
+    $image = mysql_query("SELECT * FROM IMAGE
+            WHERE ImageID=$_GET[imageID]");
+        
+    $row = mysql_fetch_array($image);
+        
+    unlink($row[ImagePathOriginal]);
+    unlink($row[ImagePathThumb]);
+        
+        
+        
+    $result = mysql_query("DELETE FROM IMAGE
+        WHERE ImageID=$_GET[imageID]");
+
+    if(!$result)
         {
             die('could not connect: ' .mysql_error());
         }
-        else
-        {
-            //echo "connected to mySQL";
-        }
-
-        $select = mysql_selectdb($db_database, $con);
-
-        if(!$select)
-        {
-            die('could not connect: ' .mysql_error());
-        }
-        else
-        {
-            //echo "Selected Database";
-        }
-        $image = mysql_query("SELECT * FROM IMAGE
-            WHERE ImageID=$_GET[imageID]");
-        
-        $row = mysql_fetch_array($image);
-        
-        unlink($row[ImagePathOriginal]);
-        unlink($row[ImagePathThumb]);
-        
-        
-        
-        $result = mysql_query("DELETE FROM IMAGE
-            WHERE ImageID=$_GET[imageID]");
-        
-        if(!$result)
-                             {
-                                 die('could not connect: ' .mysql_error());
-                             }
         
         
     echo 'This will eventually be a function to delete the listing.'; 
