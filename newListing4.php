4020436db0ee64c3068523770c24b870c051b2ed
David Pyle <pyledw@gmail.com>
2012-11-20 20:38:17 -0600

    Working on layout of some forms.

--- newListing4.php
+++ newListing4.php
@@ -59,34 +59,34 @@
                 <font class="formheader">Resident Requirements</font>
                 <tr>
                     <td>
-                        <label class="label">Will you allow a resident(s) with a criminal history?</label><br/>
+                        <label class="label">Will you allow a resident(s) with a criminal history?</label><br/>&nbsp;&nbsp;&nbsp;&nbsp;
                         Yes<input type="radio" name="criminalHistory"  value="1" <?php if($row[AllowCriminalHistory] == '1'){echo "checked='checked'";}?>/>
                         No<input type="radio" name="criminalHistory"  value="0" <?php if($row[AllowCriminalHistory] != '1'){echo "checked='checked'";}?>/>
                     </td>
                     <td>
-                        <label class="label">Will you allow only residents with a minimum salary?</label><br/>
+                        <label class="label">Will you allow only residents with a minimum salary?</label><br/>&nbsp;&nbsp;&nbsp;&nbsp;
                         Yes<input type="radio" name="minSalary"  value="1" <?php if($row[MinimumSalary] == '1'){echo "checked='checked'";}?>/>
                         No<input type="radio" name="minSalary"  value="0"<?php if($row[MinimumSalary] != '1'){echo "checked='checked'";}?> />
                     </td>
                     <td>
-                        <label class="label">Will you allow only non-smoking residents?</label><br/>
+                        <label class="label">Will you allow only non-smoking residents?</label><br/>&nbsp;&nbsp;&nbsp;&nbsp;
                         Yes<input type="radio" name="smoking"  value="1" <?php if($row[AllowSmoking] == '1'){echo "checked='checked'";}?>/>
                         No<input type="radio" name="smoking"  value="0" <?php if($row[AllowSmoking] != '1'){echo "checked='checked'";}?>/>
                     </td> 
                 </tr>
                 <tr>
                     <td>
-                        <label class="label">Will you allow cats?</label><br/>
+                        <label class="label">Will you allow cats?</label><br/>&nbsp;&nbsp;&nbsp;&nbsp;
                         Yes<input type="radio" name="cats"  value="1" <?php if($row[AllowCats] == '1'){echo "checked='checked'";}?>/>
                         No<input type="radio" name="cats"  value="0" <?php if($row[AllowCats] != '1'){echo "checked='checked'";}?>/>
                     </td>
                     <td>
-                        <label class="label">Will you allow dogs?</label><br/>
+                        <label class="label">Will you allow dogs?</label><br/>&nbsp;&nbsp;&nbsp;&nbsp;
                         Yes<input type="radio" name="dogs"  value="1" <?php if($row[AllowDogs] == '1'){echo "checked='checked'";}?>/>
                         No<input type="radio" name="dogs"  value="0" <?php if($row[AllowDogs] != '1'){echo "checked='checked'";}?>/>
                     </td>
                     <td>
-                        <label class="label">Required Pet Deposit</label><br/>
+                        <label class="label">Required Pet Deposit</label><br/>&nbsp;&nbsp;&nbsp;&nbsp;
                         Yes<input type="radio" name="petdeposit"  value="1" <?php if($row[PetDepost] == '1'){echo "checked='checked'";}?>/>
                         No<input type="radio" name="petdeposit"  value="0" <?php if($row[PetDepost] != '1'){echo "checked='checked'";}?>/>
                     </td>
@@ -94,7 +94,7 @@
                 <tr>
                     <td>
                         
-                        <label class="label">Is Pet Deposit Refundable?</label><br/>
+                        <label class="label">Is Pet Deposit Refundable?</label><br/>&nbsp;&nbsp;&nbsp;&nbsp;
                         Yes<input type="radio" name="petDepositRefundable"  value="1" <?php if($row[AllowPetDepositRefund] == '1'){echo "checked='checked'";}?>/>
                         No<input type="radio" name="petDepositRefundable"  value="0" <?php if($row[AllowPetDepositRefund] != '1'){echo "checked='checked'";}?>/>
                     </td>
