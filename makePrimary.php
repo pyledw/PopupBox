7c6fdcd80b6a987c3d270f5b796aa645e16c4343
David Pyle <pyledw@gmail.com>
2012-11-20 18:41:56 -0600

    allowing for user to set an image as main

--- makePrimary.php
+++ makePrimary.php
@@ -4,13 +4,13 @@ include_once 'imageFunctions.php';
     $con = get_dbconn("");
     
     
-        $result4 = mysql_query("UPDATE IMAGE SET ImageType=0
+        $result = mysql_query("UPDATE IMAGE SET ImageType=0
             WHERE PropertyID=$_GET[propertyID]");
 
-        $result4 = mysql_query("UPDATE IMAGE SET ImageType=1
+        $result2 = mysql_query("UPDATE IMAGE SET ImageType=1
             WHERE ImageID=$_GET[imageID]");
 
-        if(!$result4)
+        if(!$result2)
         {
             die('could not connect: ' .mysql_error());
         }
