de67bcf2feeeb7083aceb46af8a10b1e9a408130
Jason Dickinson <yodaj007@gmail.com>
2012-11-23 22:53:51 -0600

    moving old paypal_confirm.php to a doc folder for general documentation purposes

--- payListingFeeRedirect.php
+++ payListingFeeRedirect.php
@@ -1,6 +1,10 @@
+<html>
+<body>
+
 <?php
     /**
-     * This page updates the listing after the user has paid the fee.
+     * This page updates the listing after the user has paid the fee.  It then closes the paypal popup window
+	 * and redirects the main window to myHood.
      */
 include_once 'config.inc.php';
 require_once ("feeprocessing.inc.php");
@@ -8,11 +12,32 @@ $res = FeeProcessing::complete_paypal($_REQUEST['token'], $_REQUEST['PayerID']);
 
 if($res)
 {
+	// Payment was successfully completed!  Hooray!  Set the property ispaid flag now:
     FeeProcessing::set_property_ispaid($_REQUEST['token']);
-    header( 'Location: /myHood.php' );
 
+	// now close this popup window and redirect the main window back to myHood
+?>
+	<script>
+		window.onload = function(){
+			if(window.opener){
+				 window.close();
+			 }
+			else{
+				 if(top.dg.isOpen() == true){
+					 top.dg.closeFlow();
+					 return true;
+				  }
+			  }
+		};
+	</script>
+<?
 }
 else
 {
+	// what's what?  payment failed?  boooooooooo
+
 }
 ?>
+
+</body>
+</html>
