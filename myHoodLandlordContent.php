05d3362c0b0f7a1c20c18a4d5fa1753c7db13562
David Pyle <pyledw@gmail.com>
2012-11-24 13:35:49 -0600

    Fix for Desc from the most recent end time.

--- myHoodLandlordContent.php
+++ myHoodLandlordContent.php
@@ -22,7 +22,7 @@
     //Query to select the user's application using their userID number
     $result = mysql_query("SELECT * FROM PROPERTY
         WHERE UserID ='".$_SESSION['userID']."'
-            ORDER BY DatePFOEndAccept ASC");
+            ORDER BY DatePFOEndAccept DESC");
     if(!$result)
         {
             die('could not connect: ' .mysql_error());
