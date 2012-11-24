5ea3791d13f7a7d9a8669e41aae75d2493202922
Jason Dickinson <yodaj007@gmail.com>
2012-11-24 04:07:37 -0600

    Putting array index constants into quotes to eliminate errors in log

--- myHoodLandlordContent.php
+++ myHoodLandlordContent.php
@@ -29,7 +29,7 @@
     //Setting the query results into a variable
     while($row = mysql_fetch_array($result))
         {
-            displayMyListings($row[PropertyID]);
+            displayMyListings($row['PropertyID']);
 
         }
     ?>
