b1f818c8022d1fcd6b5cfb9e1ce689bbdd172408
David Pyle <pyledw@gmail.com>
2012-11-26 21:27:15 -0600

    Working on creating the move in now bid - gotten bid placed - validated to ensure it will only allow one winning bid.

--- rentItNow.php
+++ rentItNow.php
@@ -1,11 +1,29 @@
 <?php
+session_start();
 $auctionID = $_GET['auctionID'];
-echo $auctionID;
+//echo $auctionID;
+include_once 'config.inc.php';
 
+get_dbconn("");
 
-?>
+$result = mysql_query("SELECT ApplicationID FROM APPLICATION
+    WHERE UserID='".$_SESSION['userID']."'");
+if(!$result)
+        {
+            die('could not connect: ' .mysql_error());
+        }
+$row = mysql_fetch_array($result);
 
-<table>
+$result = mysql_query("SELECT BidID FROM BID
+    WHERE AuctionID='".$auctionID."' AND ApplicationID='".$row['ApplicationID']."' AND IsMoveInNowBid='1'");
+if(!$result)
+        {
+            die('could not connect: ' .mysql_error());
+        }
+if(mysql_num_rows($result) == 0)
+{
+    echo '
+        <table class="tableForm" style="margin-top:-5px;">
     <tr>
         <td colspan="2">
             Would you like to place a Move In Now?
@@ -17,3 +35,19 @@ echo $auctionID;
         </td>
     </tr>
 </table>
+
+
+';
+}
+else
+{
+    echo '<table class="tableForm" style="margin-top:-5px;">
+    <tr>
+        <td colspan="2">
+            You already have a move in now bid active on this listing.<br/>  Please wait for that Landlord to accept it.
+        </td>
+    </tr>
+</table>';
+}
+
+?>
