4f913a9d532b6888e711dbac60d52d0afa24ffc6
a.long <longas@mail.lipscomb.edu>
2012-11-27 14:52:42 -0600

    password reset main page

--- resetPassword.php
+++ resetPassword.php
@@ -10,20 +10,18 @@
 
 <!-- PAGE CONTENT -->
 
-        <h1 class="Title">Welcome to LeaseHood</h1>
+        <h1 class="Title">Forgot Password</h1>
         <hr class="Title" />
-        <h2 class="Title">"Putting the Best Residents in Rental Homes"</h2>
+        <h2 class="Title">Please enter in your email address. An email will be sent with your username and password.</h2>
 
         <div id="mainContent">
            <?php
-                echo '<form method="post" action="index.php">';
+                echo '<form method="post" action="resetPasswordRedirect.php">';
                 echo '<table>';
-                echo '<tr><td>Userid:</td>';
-                echo '<td><input type="text" name="userid"></td></tr>';
-                echo '<tr><td>Password:</td>';
-                echo '<td><input type="password" name="password"></td></tr>';
+                echo '<tr><td>Email:</td>';
+                echo '<td><input type="text" name="email"></td></tr>';
                 echo '<tr><td colspan="2" align="center">';
-                echo '<input type="submit" value="Log in"></td></tr>';
+                echo '<input type="submit" value="Send"></td></tr>';
                 echo '</table></form>';
            ?>
             
