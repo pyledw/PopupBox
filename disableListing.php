78798e6d22bb0150ba74b606a1576a477a93bcc2
a.long <longas@mail.lipscomb.edu>
2012-12-06 00:01:43 -0600

    listing cancel email

--- disableListing.php
+++ disableListing.php
@@ -12,6 +12,7 @@ if($_SESSION['type'] == '3')
 echo "redirect";
 echo $_GET['listingID'];
 echo $_GET['auctionID'];
+echo $_GET['userID'];
 
         require_once "config.inc.php";
         //Connecting to the sql database
@@ -57,6 +58,32 @@ echo $_GET['auctionID'];
         }
 }        
         header( 'Location: /myHood.php' );
+        
+        //sending email to landlord
+        //get propertyID to send email to user
+        $auctionID = $_GET['auctionID'];
+        $result5 = mysql_query("select PropertyID from AUCTION where AuctionID = '$auctionID'");
+        $propertyID = mysql_fetch_assoc($result5);
+        
+        //associate propertyid to userid and get email
+        $result6 = mysql_query("select * from PROPERTY where PropertyID = '$propertyID'");
+        $row6 = mysql_fetch_array($result6);
+        $userid = $row6['UserID'];
+        $streetAdd = $row6['Address'];
+        
+        $result7 = mysql_query("select Email from USER where UserID = '$userid'");
+        $email = mysql_fetch_assoc($result7);
+    
+        //sending email to account owner
+        $to = $email;
+        $from = "From: noReply@leasehood.com \r\n";
+        $subject = "Your Listing Has Been Removed";
+        $mesg = "Dear landlord,\n"."We regret to inform you that your listing for ".$streetAdd." has been 
+                    suspended for inappriprite use of  LeaseHood.com's Terms and Conditions.".
+                "Should you have any questions, please email us at info@LeaseHood.com.\n".
+                "Regards,\n Mark Gardner\n President|CEO";
+        
+        mail($to, $subject, $mesg, $from);
 
 
 ?>
